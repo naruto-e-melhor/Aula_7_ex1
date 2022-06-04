d94486ce0e6638c9246f3298a756fa0b6ad21fc7
naruto-e-melhor <102871291+naruto-e-melhor@users.noreply.github.com>
2022-06-03 21:59:37 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,5 @@
+let nota1 = parseInt(prompt("Digite a primeira nota:"));
+let nota2 = parseInt(prompt("Digite a segunda nota:"));
+let nota3 = parseInt(prompt("Digite a terceira nota:"));
+
+alert(`A soma das notas foi de ${nota1 + nota2 + nota3} pontos`)
